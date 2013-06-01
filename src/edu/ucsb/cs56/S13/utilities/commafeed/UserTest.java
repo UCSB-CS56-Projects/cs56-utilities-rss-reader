3c03045492dd5c1ed01677edb7081fb7b899e927
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-06-01 01:18:41 -0700

    Created tests for User.Profile parser and class

--- src/edu/ucsb/cs56/S13/utilities/commafeed/UserTest.java
+++ src/edu/ucsb/cs56/S13/utilities/commafeed/UserTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class UserTest {
   private static final String exampleSettings = "{\"language\": \"en\",\"readingMode\": \"unread\",\"readingOrder\": \"desc\",\"viewMode\": \"title\",\"showRead\": true,\"socialButtons\": true,\"scrollMarks\": true,\"theme\": \"default\",\"customCss\": null}";
-
+  private static final String exampleProfile = "{\"id\": 13161,\"name\": \"demo\",\"email\": null,\"apiKey\": null,\"password\": null,\"enabled\": true,\"admin\": false}";
 
   @Test
   public void testParseSettings() throws IOException {
@@ -20,5 +20,17 @@ public class UserTest {
     assertEquals(settings.scrollMarks, true);
     assertEquals(settings.theme, "default");
     assertEquals(settings.customCss, null);
+  }
+
+  @Test
+  public void testParseProfile() throws IOException {
+    User.Profile profile = User.parseProfile(exampleProfile);
+    assertEquals(profile.id, 13161);
+    assertEquals(profile.name, "demo");
+    assertEquals(profile.email, null);
+    assertEquals(profile.apiKey, null);
+    assertEquals(profile.password, null);
+    assertEquals(profile.enabled, true);
+    assertEquals(profile.admin, false);
   }
 }
