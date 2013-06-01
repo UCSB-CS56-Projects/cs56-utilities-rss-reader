14e6a8614c7752a9dd75d2bdc60eb2f8b2a8679e
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-05-31 19:10:37 -0700

    Added constructor for CommaFeed object

--- src/edu/ucsb/cs56/S13/utilities/commafeed/CommaFeed.java
+++ src/edu/ucsb/cs56/S13/utilities/commafeed/CommaFeed.java
@@ -1,4 +1,18 @@
 package edu.ucsb.cs56.S13.utilities.commafeed;
 
 public class CommaFeed {
+  private final String username, password;
+
+  public CommaFeed() {
+  }
+
+  public CommaFeed(String username, String password) {
+  }
+
+  public String getUsername() {
+  }
+
+  public String getPassword() {
+  }
+
 }
